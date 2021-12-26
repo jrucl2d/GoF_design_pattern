0f6e766053a795cade3bc2232d3c6ba0c0c05f04
jrucld <ysk789465@gmail.com>
2021-12-26 21:32:35 +0900

    오타 수정

--- src/main/java/com/example/guru/creational/abstractfactory/app/Application.java
+++ src/main/java/com/example/guru/creational/abstractfactory/app/Application.java
@@ -5,7 +5,7 @@ import com.example.guru.creational.abstractfactory.checkboxes.Checkbox;
 import com.example.guru.creational.abstractfactory.factories.GUIFactory;
 
 /**
- * 팩토리 사용자는 추상화된 인터페이스로 팩토리와 Product를 사용함르ㅗ 어떤 구체적인 팩토리인지 신경을 쓰지 않는다.
+ * 팩토리 사용자는 추상화된 인터페이스로 팩토리와 Product를 사용하므로 어떤 구체적인 팩토리인지 신경을 쓰지 않는다.
  */
 public class Application {
     private Button button;
