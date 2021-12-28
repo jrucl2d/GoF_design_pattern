31d5eb48f47da0c40840f2c2ca8e39cdf12c0ad3
jrucld <ysk789465@gmail.com>
2021-12-28 20:38:01 +0900

    prototype 패턴 사용

---
+++ src/main/java/com/example/guru/creational/prototype/shapes/Circle.java
@@ -0,0 +1,30 @@
+package com.example.guru.creational.prototype.shapes;
+
+import java.util.Objects;
+
+public class Circle extends Shape {
+    public int radius;
+
+    public Circle() {
+
+    }
+
+    public Circle(Circle target) {
+        super(target);
+        if (target != null) {
+            this.radius = target.radius;
+        }
+    }
+
+    @Override
+    public Shape clone() {
+        return new Circle(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Circle) || !super.equals(o)) return false;
+        Circle circle = (Circle) o;
+        return this.radius == circle.radius;
+    }
+}
