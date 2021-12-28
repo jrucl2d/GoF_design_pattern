9e632b47565c4ecf14a44a1c5566ded035ad95ad
jrucld <ysk789465@gmail.com>
2021-12-28 20:40:54 +0900

    Prototype Registry 사용

--- src/main/java/com/example/guru/creational/prototype/Demo.java
+++ src/main/java/com/example/guru/creational/prototype/Demo.java
@@ -1,5 +1,6 @@
 package com.example.guru.creational.prototype;
 
+import com.example.guru.creational.prototype.cache.BundledShapeCache;
 import com.example.guru.creational.prototype.shapes.Circle;
 import com.example.guru.creational.prototype.shapes.Rectangle;
 import com.example.guru.creational.prototype.shapes.Shape;
@@ -9,6 +10,31 @@ import java.util.List;
 
 public class Demo {
     public static void main(String[] args) {
+        BundledShapeCache cache = new BundledShapeCache();
+
+        Shape shape1 = cache.get("Big green circle");
+        Shape shape2 = cache.get("Medium blue rectangle");
+        Shape shape3 = cache.get("Medium blue rectangle");
+
+        if (shape1 != shape2 && !shape1.equals(shape2)) {
+            System.out.println("Big green circle != Medium blue rectangle");
+        } else {
+            System.out.println("Big green circle == Medium blue rectangle");
+        }
+
+        if (shape2 != shape3) {
+            System.out.println("Medium blue rectangles are two different objects");
+            if (shape2.equals(shape3)) {
+                System.out.println("And they are identical");
+            } else {
+                System.out.println("But they are not identical");
+            }
+        } else {
+            System.out.println("Rectangle objects are the same");
+        }
+    }
+
+    private static void normalDemo() {
         List<Shape> shapes = new ArrayList<>();
         List<Shape> shapesCopy = new ArrayList<>();
 
